adcbf6c842fe2167660aae707967944da4a4b3bd
ShuheiUsui <sworks.usui@gmail.com>
2018-06-11 13:09:12 +0900

    [WIP] layout

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { createStackNavigator } from "react-navigation";
+import { SafeAreaView } from "react-native";
+
 import HomeScreen from "./screens/HomeScreen";
 import NextScreen from "./screens/NextScreen";
 
 // ルーティング設定
-const RootStack = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      header: null
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        header: null
+      }
+    },
+    Next: {
+      screen: NextScreen,
+      navigationOptions: {
+        headerTitle: "Next"
+      }
     }
   },
-  Next: {
-    screen: NextScreen,
-    navigationOptions: {
-      headerTitle: "Next"
+  {
+    cardStyle: {
+      shadowColor: "transparent"
     }
   }
-});
+);
 
 export default function App() {
-  return <RootStack />;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#ddd" }}>
+      <RootStack />
+    </SafeAreaView>
+  );
 }
